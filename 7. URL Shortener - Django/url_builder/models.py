f53db091779292b9937182379666d4422f475017
tizianoadv <tiziano.nardone.advnace@gmail.com>
2022-03-23 15:47:24 +0000

    model Builder created

--- 7. URL Shortener - Django/url_builder/models.py
+++ 7. URL Shortener - Django/url_builder/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Builder(models.Model):
+    initUrl = models.URLField(max_length=200, null=False, unique=True)
+    shortcutCode = models.CharField(max_length=6, unique=True)
+    date = models.DateField(auto_now_add=True, auto_now=False, verbose_name="Date of creation of the shorcut")
+    username = models.CharField(max_length=20)
+    numberAccess = models.IntegerField(default=0)
